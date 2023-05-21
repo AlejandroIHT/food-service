07acee2e8142df55bf9b8d48e08146ff41272100
Alejandro Herrera <alejandroherrera@MacBook-Pro-de-Alejandro.local>
2023-05-21 00:16:01 -0400

    feat(create-button): create types to button component

---
+++ src/components/Button/Button.types.ts
@@ -0,0 +1,7 @@
+export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  onClick?: () => void;
+  disabled?: boolean;
+  type?: 'button' | 'submit' | 'reset';
+  className?: string;
+}
