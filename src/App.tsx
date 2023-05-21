d61219497e8ee3243d8da92aa2783bc65348fe39
Alejandro Herrera <alejandroherrera@MacBook-Pro-de-Alejandro.local>
2023-05-21 18:04:53 -0400

    feat(create-store): add provider from redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import MainRoutes from "./routes/MainRoutes";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <MainRoutes />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <MainRoutes />
+        </BrowserRouter>
+      </Provider>
     </QueryClientProvider>
   );
 }
