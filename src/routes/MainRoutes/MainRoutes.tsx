21e46a9907556bb6dd1707be2bbbe6a5f8004976
Alejandro Herrera <alejandroherrera@MacBook-Pro-de-Alejandro.local>
2023-05-21 16:27:43 -0400

    feat(product-list-integration): enable product list route

--- src/routes/MainRoutes/MainRoutes.tsx
+++ src/routes/MainRoutes/MainRoutes.tsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "../../components/Layout";
-//import ProductList from "../../pages/ProductList";
+import ProductList from "../../pages/ProductList";
 
 const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        {/* TODO: Create product list service */}
-        {/*<Route index element={<ProductList />} />*/}
+        <Route index element={<ProductList />} />
+        {/* TODO: Create Checkout component */}
         {/*<Route path="checkout" element={<h1>Checkout</h1>} />*/}
       </Route>
     </Routes>
