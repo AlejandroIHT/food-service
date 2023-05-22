53bbdf446b3632e5845a2a7d5553b369eea4740e
Alejandro Herrera <alejandroherrera@MacBook-Pro-de-Alejandro.local>
2023-05-22 17:10:12 -0400

    feat(create-test): update layout test

--- src/components/Layout/Layout.test.tsx
+++ src/components/Layout/Layout.test.tsx
@@ -1,9 +1,19 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
+import * as redux from "react-redux";
 import Layout from "./Layout";
 
+jest.mock("../Header");
+jest.mock("react-redux");
+const useDispatchSpy = jest.spyOn(redux, "useDispatch");
+const mockDispatchFn = jest.fn();
+
 describe("Layout", () => {
+  beforeEach(() => {
+    useDispatchSpy.mockReturnValue(mockDispatchFn);
+  });
   test("renders correctly", () => {
     const { container } = render(<Layout />);
     expect(container.querySelector(".layout")).toBeInTheDocument();
+    expect(mockDispatchFn).toHaveBeenCalledTimes(1);
   });
 });
