45ea70986b6610d9b3a52afe30add26aae5d9624
Alejandro Herrera <alejandroherrera@MacBook-Pro-de-Alejandro.local>
2023-05-22 11:35:50 -0400

    fix(add-interactions-dropdown): update types and name of the state

--- src/slices/cartSlice.ts
+++ src/slices/cartSlice.ts
@@ -1,70 +1,70 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { Amiibo } from '../services/productList/productList.types';
-import { ProducrListInCart } from '../pages/ProductList/ProductList.types';
+import { ProductDetailInTheCart } from '../pages/ProductList/ProductList.types';
 
 export const cartSlice = createSlice({
   name: 'cart',
   initialState: {
-    cart: [] as ProducrListInCart[],
+    cartItems: [] as ProductDetailInTheCart[],
   },
   reducers: {
     addToCart: (state, action) => {
       const product = action.payload as Amiibo;
       const productWithQuantity = { ...product, quantity: 1 };
       const cartFromLocalStorage = JSON.parse(localStorage.getItem("cart") || "[]");
-      const indexOfProductInCart = cartFromLocalStorage.findIndex((item: ProducrListInCart) => 
+      const indexOfProductInCart = cartFromLocalStorage.findIndex((item: ProductDetailInTheCart) => 
         item.tail === product.tail
       );
 
       if (indexOfProductInCart === -1) {
         localStorage.setItem(
           "cart",
-          JSON.stringify([productWithQuantity, ...cartFromLocalStorage])
+          JSON.stringify([...cartFromLocalStorage, productWithQuantity])
         );
-        state.cart = [productWithQuantity, ...cartFromLocalStorage];
+        state.cartItems = [...cartFromLocalStorage, productWithQuantity];
       } else {
         cartFromLocalStorage[indexOfProductInCart].quantity += 1;
         localStorage.setItem("cart", JSON.stringify(cartFromLocalStorage));
-        state.cart = cartFromLocalStorage;
+        state.cartItems = cartFromLocalStorage;
       }
     },
     addAllToCartFromLocalStorage: (state) => {
       const cartFromLocalStorage = JSON.parse(localStorage.getItem("cart") || "[]");
 
       if (cartFromLocalStorage.length !== 0) {
-        state.cart = [...cartFromLocalStorage];
+        state.cartItems = [...cartFromLocalStorage];
       }
     },
     removeItemFromCart: (state, action) => {
       const tail = action.payload as string;
       const cartFromLocalStorage = JSON.parse(localStorage.getItem("cart") || "[]");
-      const indexOfProductInCart = cartFromLocalStorage.findIndex((item: ProducrListInCart) => 
+      const indexOfProductInCart = cartFromLocalStorage.findIndex((item: ProductDetailInTheCart) => 
         item.tail === tail
       );
 
       if (indexOfProductInCart !== -1) {
         cartFromLocalStorage.splice(indexOfProductInCart, 1);
         localStorage.setItem("cart", JSON.stringify(cartFromLocalStorage));
-        state.cart = cartFromLocalStorage;
+        state.cartItems = cartFromLocalStorage;
       }
     },
     increaseQuantity: (state, action) => {
-      const { tail } = action.payload as ProducrListInCart;
+      const { tail } = action.payload;
       const cartFromLocalStorage = JSON.parse(localStorage.getItem("cart") || "[]");
-      const indexOfProductInCart = cartFromLocalStorage.findIndex((item: ProducrListInCart) => 
+      const indexOfProductInCart = cartFromLocalStorage.findIndex((item: ProductDetailInTheCart) => 
         item.tail === tail
       );
 
       if (indexOfProductInCart !== -1) {
         cartFromLocalStorage[indexOfProductInCart].quantity++;
         localStorage.setItem("cart", JSON.stringify(cartFromLocalStorage));
-        state.cart = cartFromLocalStorage;
+        state.cartItems = cartFromLocalStorage;
       }
     },
     decreaseQuantity: (state, action) => {
-      const { tail } = action.payload as ProducrListInCart;
+      const { tail } = action.payload;
       const cartFromLocalStorage = JSON.parse(localStorage.getItem("cart") || "[]");
-      const indexOfProductInCart = cartFromLocalStorage.findIndex((item: ProducrListInCart) => 
+      const indexOfProductInCart = cartFromLocalStorage.findIndex((item: ProductDetailInTheCart) => 
         item.tail === tail
       );
 
@@ -78,7 +78,7 @@ export const cartSlice = createSlice({
         }
 
         localStorage.setItem("cart", JSON.stringify(cartFromLocalStorage));
-        state.cart = cartFromLocalStorage;
+        state.cartItems = cartFromLocalStorage;
       }
     }
   },
